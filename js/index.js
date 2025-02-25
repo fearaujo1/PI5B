af61fbf53858d56df5536e272632997524c32933
“Mariana <marianagabiely@edu.unifil.br>
2025-02-25 20:53:50 -0300

    Começando o processo de funcionalidade da tela de cadastro

--- js/index.js
+++ js/index.js
@@ -6,17 +6,8 @@ document.getElementById("redefinir-senha").addEventListener("click", function(){
     window.location.href = "./redefinirSenha.html"
 });
 
-const script_do_google = 'https://script.google.com/macros/s/AKfycbzFX7joMaZA3oyQkLZr-_IfepxJTjWzXXnEhRaUiQwyfFgUJ0q92itO-TiqWkQGE4WNIQ/exec';
-const dados_do_formulário = document.getElementById("acessar");
-
-dados_do_formulário.addEventListener('submit', function (e){
-    e.preventDefault();
-    fetch(script_do_google, {method: 'POST', body: new FormData(dados_do_formulário) })
-    .then(response =>{
-        alert('Dados Cadastrados', response);
-        dados_do_formulário.reset();
-    })
-    .catch(error =>
-
-        console.error('Erro ao cadastrar', error));
-})
+const handleSubmit = (event) => {
+    event.preventDefault(); 
+    alert('cu')
+}
+document.querySelector('button').addEventListener('click', handleSubmit);
