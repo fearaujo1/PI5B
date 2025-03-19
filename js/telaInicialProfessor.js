5f0ebc96d2cb3889f3ac46802296eb2b247123de
Renato Barreto <renato.barreto@edu.unifil.br>
2025-03-19 20:13:34 -0300

    Estruturando a pagina inicial e de frequencia

--- js/telaInicialProfessor.js
+++ js/telaInicialProfessor.js
@@ -8,6 +8,10 @@ document.getElementById("buttomNotas").addEventListener("click", function(){
     window.location.href = "./registroNotas.html"
  });
 
+ document.getElementById("buttomFaltas").addEventListener("click", function(){
+    window.location.href = "./registroFreq.html"
+ });
+
 document.addEventListener("DOMContentLoaded", function () {
     const imgMenu = document.querySelector('.imgMenu');
     const boxMenu = document.querySelector(".boxMenu");
